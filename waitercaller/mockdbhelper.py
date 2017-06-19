6d30c95cd3809c9cb6996acabd5fbb42d2a0e5e0
Vinicius Ferreira de Souza <vinicius.fk9@gmail.com>
2017-06-19 16:34:21 -0300

    Adding User Account Control using Bootstrap

---
+++ waitercaller/mockdbhelper.py
@@ -0,0 +1,15 @@
+MOCK_USERS = [{"email": "test@example.com",
+               "salt": "8Fb23mMNHD5Zb8pr2qWA3PE9bH0=",
+               "hashed": "1736f83698df3f8153c1fbd6ce2840f8aace4f200771a4667263"
+                         "5374073cc876cf0aa6a31f780e576578f791b5555b50df463"
+                         "03f0c3a7f2d21f91aa1429ac22e"}]
+
+class MockDBHelper:
+    def get_user(self, email):
+        user = [x for x in MOCK_USERS if x.get("email") == email]
+        if user:
+            return user[0]
+        return None
+
+    def add_user(self, email, salt, hashed):
+        MOCK_USERS.append({"email": email, "salt": salt, "hashed": hashed})
