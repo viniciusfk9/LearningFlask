686eb62c40af0b1b0c1f25620d63e8591e46ad51
Vinicius Ferreira de Souza <vinicius.fk9@gmail.com>
2017-06-25 04:15:29 -0300

    Add Admin dashboard with Flask-Admin

--- votr/votr.py
+++ votr/votr.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request, flash, session, redirect, url_for, jsonify
+from flask import Flask, render_template, request, flash, session, redirect, \
+    url_for, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_migrate import Migrate
-from models import db, Users, Polls, Topics, Options
+from models import db, Users, Polls, Topics, Options, UserPolls
+from admin import AdminView, TopicView
+
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 votr = Flask(__name__)
 
@@ -13,6 +18,12 @@ db.init_app(votr)
 db.create_all(app=votr)
 
 migrate = Migrate(votr, db, render_as_batch=True)
+
+admin = Admin(votr, name='Dashboard', index_view=TopicView(
+    Topics, db.session, url='/admin', endpoint='admin'))
+admin.add_view(AdminView(Users, db.session))
+admin.add_view(AdminView(Polls, db.session))
+admin.add_view(AdminView(Options, db.session))
 
 
 @votr.route('/')
@@ -68,7 +79,7 @@ def login():
         # user wasn't found in the database
         flash('Username or password is incorrect please try again', 'error')
 
-    return redirect(url_for('home'))
+    return redirect(request.args.get('next') or url_for('home'))
 
 
 @votr.route('/logout')
@@ -99,7 +110,8 @@ def api_polls():
                 return jsonify({'message': 'value for {} is empty'.format(key)})
 
         title = poll['title']
-        options_query = lambda option: Options.query.filter(Options.name.like(option))
+        options_query = lambda option: Options.query\
+            .filter(Options.name.like(option))
 
         options = [Polls(option=Options(name=option))
                    if options_query(option).count() == 0
@@ -137,11 +149,27 @@ def api_poll_vote():
     poll_title, option = (poll['poll_title'], poll['option'])
 
     join_tables = Polls.query.join(Topics).join(Options)
-    # filter options
-    option = join_tables.filter(Topics.title.like(poll_title)).filter(Options.name.like(option)).first()
 
-    # increment vote_count by 1 if the option was found
+    # Get topic and username from the database
+    topic = Topics.query.filter_by(title=poll_title).first()
+    user = Users.query.filter_by(username=session['user']).first()
+
+    # filter options
+    option = join_tables.filter(Topics.title.like(poll_title))\
+        .filter(Options.name.like(option)).first()
+
+    # check if the user has voted on this poll
+    poll_count = UserPolls.query.filter_by(topic_id=topic.id)\
+        .filter_by(user_id=user.id).count()
+    if poll_count > 0:
+        return jsonify({'message': 'Sorry! multiple votes are not allowed'})
+
     if option:
+        # record user and poll
+        user_poll = UserPolls(topic_id=topic.id, user_id=user.id)
+        db.session.add(user_poll)
+
+        # increment vote_count by 1 if the option was found
         option.vote_count += 1
         db.session.commit()
 
